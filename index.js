e5488239dae3144be80bd45be95c38bb84d73b2f
Giacomo-Spano <giacomo.spano@libero.it>
2022-02-07 20:14:44 +0100

    2 files modified

--- index.js
+++ index.js
@@ -1,52 +1,122 @@
 // Import stylesheets
-import './style.css';
-// Firebase App (the core Firebase SDK) is always required
-import { initializeApp } from 'firebase/app';
+//import './style.css';
 
-// Add the Firebase products and methods that you want to use
-import {} from 'firebase/auth';
-import {} from 'firebase/firestore';
+// Write Javascript code!
+//const appDiv = document.getElementById('app');
+//appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-import * as firebaseui from 'firebaseui';
 
-// Document elements
-const startRsvpButton = document.getElementById('startRsvp');
-const guestbookContainer = document.getElementById('guestbook-container');
 
-const form = document.getElementById('leave-message');
-const input = document.getElementById('message');
-const guestbook = document.getElementById('guestbook');
-const numberAttending = document.getElementById('number-attending');
-const rsvpYes = document.getElementById('rsvp-yes');
-const rsvpNo = document.getElementById('rsvp-no');
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-let rsvpListener = null;
-let guestbookListener = null;
+// Initialize Cloud Firestore through Firebase
+import { getFirestore } from "firebase/firestore"
+import { collection, getDocs } from "firebase/firestore"; 
+import { collection, addDoc } from "firebase/firestore"; 
+import { collection, query, where } from "firebase/firestore";
+import { addDoc, collection } from "firebase/firestore"; 
 
-let db, auth;
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBS5DEp1211y96IZ2vr-e_p_vrwzWD1tzY",
+  authDomain: "villamariaweb-2f399.firebaseapp.com",
+  projectId: "villamariaweb-2f399",
+  storageBucket: "villamariaweb-2f399.appspot.com",
+  messagingSenderId: "1074313712213",
+  appId: "1:1074313712213:web:c92c0b62b648f8f13f7adc",
+  measurementId: "G-PMXD5J0FNK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const db = getFirestore(app);
+
+
+// Get a list of cities from your database
+async function getCities(db) {
+
+  console.log("step getCities");
+
+  const citiesCol = collection(db, 'racc');
+  const citySnapshot = await getDocs(citiesCol);
+  const cityList = citySnapshot.docs.map(doc => doc.data());
+  return cityList;
+}
 
 async function main() {
   // Add Firebase project configuration object here
-  const firebaseConfig = {};
+  //const firebaseConfig = {};
 
-  // initializeApp(firebaseConfig);
+  console.log("step 1");
 
-  // FirebaseUI config
-  const uiConfig = {
-    credentialHelper: firebaseui.auth.CredentialHelper.NONE,
-    signInOptions: [
-      // Email / Password Provider.
-      EmailAuthProvider.PROVIDER_ID,
-    ],
-    callbacks: {
-      signInSuccessWithAuthResult: function (authResult, redirectUrl) {
-        // Handle sign-in.
-        // Return false to avoid redirect.
-        return false;
-      },
-    },
-  };
+  
 
-  // const ui = new firebaseui.auth.AuthUI(auth);
+  /*try {
+    const docRef = await addDoc(collection(db, "users"), {
+      first: "Ada",
+      last: "Lovelace",
+      born: 1815
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }*/
+
+  // Add a second document with a generated ID.
+
+
+/*try {
+  const docRef = await addDoc(collection(db, "users"), {
+    first: "Alan",
+    middle: "Mathison",
+    last: "Turing",
+    born: 1912
+  });
+
+  console.log("Document written with ID: ", docRef.id);
+} catch (e) {
+  console.error("Error adding document: ", e);
+}*/
+
+  const q = query(collection(db, "users"), where("first", "==", 'Alan'));
+
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    console.log(doc.id, " => ", doc.data().born, ".", doc.data().first, ".", doc.data().last, ".", );
+  });
+
+  console.log("first query executed");
+
+  const querySnapshot2 = await getDocs(collection(db, "users"));
+  querySnapshot2.forEach((doc) => {
+    console.log(`${doc.id} => ${doc.data()}`);
+  });
+
+  const querySnapshot3 = await getDocs(collection(db, "cities"));
+  querySnapshot3.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    console.log(doc.id, " => ", doc.data().born, " ", doc.data().first);
+  });
+  
+
+
+  //getCities(db);
+  
+  console.log("step 3");
+
+
+
 }
 main();
+
+
+
